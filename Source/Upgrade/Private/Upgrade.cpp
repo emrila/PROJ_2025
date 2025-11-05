6bc8b514e36dfb0fff9ca8147cd4da68679c78d1
Emma <86224710+emrila@users.noreply.github.com>
2025-11-05 15:54:01 +0100

    Added Upgrade module

---
+++ Source/Upgrade/Private/Upgrade.cpp
@@ -0,0 +1,20 @@
+﻿#include "Upgrade.h"
+
+#include "Dev/UpgradeLog.h"
+
+#define LOCTEXT_NAMESPACE "FUpgradeModule"
+
+
+void FUpgradeModule::StartupModule()
+{
+    UPGRADE_DISPLAY(TEXT("Upgrade module has started!") );
+}
+
+void FUpgradeModule::ShutdownModule()
+{
+    UPGRADE_DISPLAY(TEXT("Shutdown module has started!") );
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FUpgradeModule, Upgrade)
---
+++ Source/Upgrade/Upgrade.Build.cs
@@ -0,0 +1,27 @@
+﻿using UnrealBuildTool;
+
+public class Upgrade : ModuleRules
+{
+    public Upgrade(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core", "Engine",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore",
+                "UMG"
+            }
+        );
+    }
+}
