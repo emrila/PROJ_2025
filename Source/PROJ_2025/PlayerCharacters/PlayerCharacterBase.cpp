dd94724b99ab4612280fc68e372da478a28520d0
Basir Ghani <144438561+basirghani@users.noreply.github.com>
2025-11-07 21:30:01 +0100

    Added: Base player class (only testing atm)

---
+++ Source/PROJ_2025/PlayerCharacters/PlayerCharacterBase.cpp
@@ -0,0 +1,34 @@
+﻿#include "PlayerCharacterBase.h"
+
+
+APlayerCharacterBase::APlayerCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void APlayerCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void APlayerCharacterBase::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+}
+
+void APlayerCharacterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APlayerCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+UAttackComponent* APlayerCharacterBase::GetFirstAttackComponent() const
+{
+	return nullptr;
+}
+
---
+++ Source/PROJ_2025/PlayerCharacters/PlayerCharacterBase.h
@@ -0,0 +1,48 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PlayerCharacterBase.generated.h"
+
+class UAttackComponent;
+DECLARE_LOG_CATEGORY_EXTERN(PlayerBaseLog, Log, All);
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class PROJ_2025_API APlayerCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
+
+public:
+	APlayerCharacterBase();
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UAttackComponent* GetFirstAttackComponent() const;
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	UPROPERTY()
+	UAttackComponent* FirstAttackComponent;
+
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+	
+	
+};
