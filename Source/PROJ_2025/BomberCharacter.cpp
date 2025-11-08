80bc84c3bf00c3b744711fe083d402697d250d72
herman6006 <144448076+herman6006@users.noreply.github.com>
2025-11-08 11:50:36 +0100

    Refactor: Fiende base class

--- Source/PROJ_2025/BomberCharacter.cpp
+++ Source/PROJ_2025/BomberCharacter.cpp
@@ -10,10 +10,8 @@ void ABomberCharacter::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
 	DOREPLIFETIME(ABomberCharacter, bIsDiving);
-	DOREPLIFETIME(ABomberCharacter, Health);
 }
 
-// Sets default values
 ABomberCharacter::ABomberCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
@@ -21,44 +19,13 @@ ABomberCharacter::ABomberCharacter()
 
 }
 
-// Called when the game starts or when spawned
-void ABomberCharacter::BeginPlay()
+void ABomberCharacter::HandleDeath()
 {
-	Super::BeginPlay();
-	
+	Server_SpawnExplosion(GetActorLocation(),GetActorRotation());
+
+	Super::HandleDeath();
 }
 
-// Called every frame
-void ABomberCharacter::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void ABomberCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
-
-float ABomberCharacter::TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent,
-	class AController* EventInstigator, AActor* DamageCauser)
-{
-	Health -= DamageAmount;
-	LaunchCharacter(GetActorForwardVector() * -1555, false, false);
-	if (Health <= 0)
-	{
-		if (CombatManager && CombatManager->HasAuthority())
-		{
-			CombatManager->RegisterEnemyDeath();
-			UE_LOG(LogTemp, Log, TEXT("Combat Death!"));
-		}
-		Server_SpawnExplosion(GetActorLocation(),GetActorRotation());
-		Destroy();
-	}
-	return Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
-}
 
 void ABomberCharacter::Server_SpawnExplosion_Implementation(FVector SpawnLocation, FRotator SpawnRotation)
 {
---
+++ Source/PROJ_2025/EnemyBase.h
@@ -0,0 +1,35 @@
+// Furkan approves of this
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CombatManager.h"
+#include "GameFramework/Character.h"
+#include "EnemyBase.generated.h"
+
+UCLASS()
+class PROJ_2025_API AEnemyBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AEnemyBase();
+
+	UFUNCTION(BlueprintCallable)
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent,
+		class AController* EventInstigator, AActor* DamageCauser) override;
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY()
+	ACombatManager* CombatManager;
+
+protected:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated, Category="Stats")
+	float Health;
+
+	virtual void HandleDeath();
+	
+
+};
