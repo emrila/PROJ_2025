4d4633f5e38a5f7c693a3681474f96d260de01be
herman6006 <144448076+herman6006@users.noreply.github.com>
2025-11-08 12:27:08 +0100

    Reorganized enemy folder

---
+++ Source/PROJ_2025/CactusCharacter.h
@@ -0,0 +1,17 @@
+// Furkan approves of this
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyBase.h"
+#include "CactusCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJ_2025_API ACactusCharacter : public AEnemyBase
+{
+	GENERATED_BODY()
+	
+};
