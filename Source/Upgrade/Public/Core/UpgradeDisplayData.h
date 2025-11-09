2025e390898be20bb1a6eacff1ae0548ecad33eb
Emma <emmarichterlarsson@gmail.com>
2025-11-09 01:19:16 +0100

    Add AttributeData struct and extend upgrade display data

--- Source/Upgrade/Public/Core/UpgradeDisplayData.h
+++ Source/Upgrade/Public/Core/UpgradeDisplayData.h
@@ -11,13 +11,43 @@ struct FUpgradeDisplayData
 	GENERATED_BODY()
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	FText Title;
+	FText Title = FText::FromString("Upgrade Title");
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	FText Description;
+	FText Description = FText::FromString("Upgrade Description");
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	TSoftObjectPtr<UTexture2D> Icon = TSoftObjectPtr<UTexture2D>(FSoftObjectPath(TEXT("/Engine/VREditor/Devices/Vive/UE4_Logo.UE4_Logo")));
 
-	// TODO: Add distinguisher
+	FName RowName = NAME_None;
+};
+
+USTRUCT(BlueprintType)
+struct FPlayerUpgradeDisplayEntry
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName PlayerType = NAME_None;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FUpgradeDisplayData> UpgradeDataArray = {};
+};
+
+USTRUCT(BlueprintType)
+struct UPGRADE_API FAttributeUpgradeData : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+	virtual void OnDataTableChanged(const UDataTable* InDataTable, const FName InRowName) override
+	{
+		UpgradeDisplayData.RowName = InRowName;
+	}
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(ToolTip = "-1 = Infinite upgrades"))
+	FUpgradeDisplayData UpgradeDisplayData;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(ToolTip = "-1 = Infinite upgrades"))
+	int32 MaxNumberOfUpgrades = -1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(ToolTip = "Used as: value += InitialValue * Multiplier"))
+	float Multiplier = 0.1f; //percental increase per upgrade level
 };
