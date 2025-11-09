9311b3eb89cbd757174066b626d5ccc3c6813db0
Emma <emmarichterlarsson@gmail.com>
2025-11-09 01:17:49 +0100

    Add UPGRADE_HI_FROM logging macro

--- Source/Upgrade/Private/Dev/UpgradeLog.h
+++ Source/Upgrade/Private/Dev/UpgradeLog.h
@@ -6,3 +6,5 @@ DECLARE_LOG_CATEGORY_EXTERN(CLogUpgrade, Log, All);
 #define UPGRADE_DISPLAY(Text, ...) UE_LOG(CLogUpgrade, Display, Text, ##__VA_ARGS__)
 #define UPGRADE_WARNING(Text, ...) UE_LOG(CLogUpgrade, Warning, Text, ##__VA_ARGS__)
 #define UPGRADE_ERROR(Text, ...) UE_LOG(CLogUpgrade, Error, Text, ##__VA_ARGS__)
+
+#define UPGRADE_HI_FROM(...) UE_LOG(CLogUpgrade, Display, TEXT("%hs: Called"), ##__VA_ARGS__)
