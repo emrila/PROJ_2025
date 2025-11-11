8be044178f69fd01e95ce11ed24cdf375b8aa6ec
Emma <86224710+emrila@users.noreply.github.com>
2025-11-11 20:52:17 +0100

    Add interact interface

---
+++ Source/Upgrade/Private/World/Interact.cpp
@@ -0,0 +1,7 @@
+﻿// Furkan approves of this
+
+
+#include "World/Interact.h"
+
+
+// Add default functionality here for any IInteract functions that are not pure virtual.
---
+++ Source/Upgrade/Public/World/Interact.h
@@ -0,0 +1,31 @@
+﻿// Furkan approves of this
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Interact.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UInteract : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class UPGRADE_API IInteract
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION( BlueprintNativeEvent, BlueprintCallable, Category="Interactable" )
+	void OnInteract(UObject* Interactor);
+	virtual void OnInteract_Implementation(UObject* Interactor){}
+	
+	UFUNCTION( BlueprintNativeEvent, BlueprintCallable, Category="Interactable" )
+	bool CanInteract(UObject* Interactor);
+	virtual bool CanInteract_Implementation(UObject* Interactor)
+	{
+		return true;
+	}
+};
