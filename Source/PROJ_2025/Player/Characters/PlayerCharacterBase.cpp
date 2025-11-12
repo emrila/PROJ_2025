2dfdd314a772f48b3de8cc75fb0c6aa23ac59d74
Basir Ghani <144438561+basirghani@users.noreply.github.com>
2025-11-12 12:37:35 +0100

    Improve input validation and remove HealthBarWidget
    Added null checks for FirstAttackComponent and PlayerInputComponent in PlayerCharacterBase to prevent errors. Removed HealthBarWidget from PlayerControllerBase, including its initialization and property declaration, consolidating UI handling to HudWidget.

--- Source/PROJ_2025/Player/Characters/PlayerCharacterBase.cpp
+++ Source/PROJ_2025/Player/Characters/PlayerCharacterBase.cpp
@@ -89,6 +89,7 @@ void APlayerCharacterBase::UseFirstAttackComponent()
 	if (!FirstAttackComponent)
 	{
 		UE_LOG(PlayerBaseLog, Error, TEXT("APlayerCharacterBase::UseFirstAttackComponent, FirstAttackComp is Null"));
+		return;
 	}
 
 	GetFirstAttackComponent()->StartAttack();
@@ -106,6 +107,11 @@ void APlayerCharacterBase::Tick(float DeltaTime)
 
 void APlayerCharacterBase::SetupPlayerInputComponent_Implementation(class UInputComponent* PlayerInputComponent)
 {
+	if (!PlayerInputComponent)
+	{
+		UE_LOG(PlayerBaseLog, Error, TEXT("APlayerCharacterBase::SetupPlayerInputComponent, PlayerInputComponent is NULL"));
+		return;
+	}
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 	if (UEnhancedInputComponent* EnhancedInputComponent =
