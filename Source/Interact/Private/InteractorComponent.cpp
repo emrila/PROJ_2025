dd4866c279793e520fe3f4086b8c80c10ef6763a
Emma <emmarichterlarsson@gmail.com>
2025-11-12 17:10:54 +0100

    Add InteractorComponent
    For checking and storing Interactables, and state handling

---
+++ Source/Interact/Private/InteractorComponent.cpp
@@ -0,0 +1,127 @@
+﻿// Furkan approves of this
+
+
+#include "InteractorComponent.h"
+
+#include "Dev/InteractLog.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Net/UnrealNetwork.h"
+
+namespace InteractUtil
+{
+	bool Trace(AActor* Owner, const float InteractionRadius, const float InteractionDistance, FHitResult& Hit, const EDrawDebugTrace::Type DebugType = EDrawDebugTrace::ForOneFrame)
+	{
+		FVector Start;
+		FRotator ViewRot;
+		Owner->GetActorEyesViewPoint(Start, ViewRot);
+		const FVector End = Start + ViewRot.Vector() * InteractionDistance;
+		const ETraceTypeQuery TraceChannel = UEngineTypes::ConvertToTraceType(ECC_Visibility);
+		return UKismetSystemLibrary::SphereTraceSingle(Owner, Start, End, InteractionRadius, TraceChannel,false,{Owner}, DebugType,Hit,true);
+	}
+}
+
+UInteractorComponent::UInteractorComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.TickInterval = 0.2f;
+
+	InteractionRadius = 40.f;
+	InteractionDistance = 1000.f;
+	bInteracting = false;
+}
+
+
+void UInteractorComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	SetInteracting(false);
+	SetTargetInteractable(nullptr);
+}
+
+
+void UInteractorComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	TraceForInteractable();
+
+}
+
+void UInteractorComponent::TraceForInteractable()
+{
+	auto IsInteractable = [this] (UObject* Object) -> bool{
+		return Object && Object->Implements<UInteractable>() && Execute_CanInteract(Object);
+	};
+
+	if (bInteracting)
+	{
+		INTERACT_WARNING( TEXT("Currently interacting, will not trace for interactable"));
+		return;
+	}
+
+	AActor* Owner = GetOwner();
+	if (!Owner) // Should not happen though
+	{
+		return;
+	}
+	FHitResult Hit;
+	if (!InteractUtil::Trace(Owner,InteractionRadius, InteractionDistance, Hit))
+	{
+		//INTERACT_DISPLAY( TEXT("Nothing found in trace"));
+		return;
+	}
+
+	if (IsInteractable(Hit.GetActor()))
+	{
+		SetTargetInteractable(Hit.GetActor());
+		INTERACT_DISPLAY( TEXT("Found Actor interactable: %s"), *GetNameSafe(Hit.GetActor()));
+	}
+	else if (IsInteractable(Hit.GetComponent()))
+	{
+		SetTargetInteractable(Hit.GetComponent());
+		INTERACT_DISPLAY( TEXT("Found Component interactable: %s"), *GetNameSafe(Hit.GetComponent()));
+	}
+}
+
+void UInteractorComponent::ClearInteractable()
+{
+	INTERACT_DISPLAY(TEXT("Clearing interactable"));
+	SetInteracting(false);
+	SetTargetInteractable(nullptr);
+}
+
+void UInteractorComponent::OnInteract_Implementation(UObject* Interactor)
+{
+	if (!Execute_CanInteract(this))
+	{
+		INTERACT_WARNING( TEXT("Trying to interact, but cannot interact! IsInteracting: %s, TargetInteractable: %s"), bInteracting ? TEXT("true") : TEXT("false"), *GetNameSafe(TargetInteractable.GetObject()));
+		return;
+	}
+
+	SetInteracting(true);
+
+	if (!TargetInteractable.GetObject())
+	{
+		INTERACT_WARNING( TEXT("TargetInteractable is null when trying to interact!"));
+		ClearInteractable();
+		return;
+	}
+
+	Execute_OnInteract(TargetInteractable.GetObject(), this);
+}
+
+bool UInteractorComponent::CanInteract_Implementation()
+{
+	return !bInteracting && TargetInteractable.GetObject();
+}
+
+void UInteractorComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(UInteractorComponent, bInteracting);
+}
+
+void UInteractorComponent::SetTargetInteractable(const TScriptInterface<IInteractable> InTargetInteractable)
+{
+	TargetInteractable = InTargetInteractable;
+	INTERACT_DISPLAY( TEXT("Setting target interactable to: %s"), *GetNameSafe(TargetInteractable.GetObject()));
+}
---
+++ Source/Interact/Public/InteractorComponent.h
@@ -0,0 +1,87 @@
+﻿// Furkan approves of this
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interactable.h"
+#include "Components/ActorComponent.h"
+#include "InteractorComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class INTERACT_API UInteractorComponent : public UActorComponent, public IInteractable
+{
+	GENERATED_BODY()
+
+public:
+	UInteractorComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable, Category="Interaction|Interactor")
+	void TraceForInteractable();
+
+	UFUNCTION(BlueprintCallable, Category="Interaction|Interactor")
+	void ClearInteractable();
+
+	virtual void OnInteract_Implementation(UObject* Interactor) override;
+	virtual bool CanInteract_Implementation() override;
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Getter, Setter, Category="Interact|Trace")
+	float InteractionRadius;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Getter, Setter, Category="Interact|Trace")
+	float InteractionDistance;
+
+	UPROPERTY(Replicated, BlueprintReadOnly, Category="Interact|Interactor")
+	bool bInteracting;
+
+	UPROPERTY(BlueprintReadOnly, Category="Interact|Interactor")
+	TScriptInterface<IInteractable> TargetInteractable;
+
+public:
+	UFUNCTION(BlueprintCallable, Category="Interact|Interactor")
+	void SetInteracting(const bool bInInteracting)
+	{
+		bInteracting = bInInteracting;
+	}
+
+	UFUNCTION(BlueprintCallable, Category="Interact|Interactor")
+	void SetTargetInteractable(const TScriptInterface<IInteractable> InTargetInteractable);
+
+	UFUNCTION(BlueprintPure, Category="Interact|Interactor")
+	TScriptInterface<IInteractable> GetTargetInteractable() const
+	{
+		return TargetInteractable;
+	}
+
+	UFUNCTION(BlueprintCallable, Category="Interact|Trace")
+	void SetInteractionRadius(const float NewRadius)
+	{
+		InteractionRadius = NewRadius;
+	}
+
+	UFUNCTION(BlueprintPure, Category="Interact|Trace")
+	float GetInteractionRadius() const
+	{
+		return InteractionRadius;
+	}
+
+	UFUNCTION(BlueprintCallable, Category="Interact|Trace")
+	void SetInteractionDistance(const float NewDistance)
+	{
+		InteractionDistance = NewDistance;
+	}
+
+	UFUNCTION(BlueprintPure, Category="Interact|Trace")
+	float GetInteractionDistance() const
+	{
+		return InteractionDistance;
+	}
+};
