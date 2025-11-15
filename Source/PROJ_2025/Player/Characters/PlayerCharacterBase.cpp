66bb88e850e7616365bd6ae982e31a259d88974f
Basir Ghani <144438561+basirghani@users.noreply.github.com>
2025-11-15 15:49:01 +0100

    Reorganized: PlayerCharacterBase

--- Source/PROJ_2025/Player/Characters/PlayerCharacterBase.cpp
+++ Source/PROJ_2025/Player/Characters/PlayerCharacterBase.cpp
@@ -21,10 +21,10 @@ APlayerCharacterBase::APlayerCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
-	// Use controller desired yaw rotation so the character rotates with mouse input
+	// Use the controller desired yaw rotation so the character rotates with mouse input
 	bUseControllerRotationYaw = true;
 
-	//Adjusting rotation rates, can be changed in the editor to suit gameplay
+	//Adjusting rotation rates, this can be changed in the editor to suit gameplay
 	GetCharacterMovement()->RotationRate = FRotator(250.f, 250.0f, 250.0f);
 
 	// Create a camera boom (pulls in towards the player if there is a collision)
@@ -46,202 +46,10 @@ APlayerCharacterBase::APlayerCharacterBase()
 	PlayerNameTagWidgetComponent->AddLocalOffset(FVector(0.0f, 0.0f, 100.0f));
 }
 
-void APlayerCharacterBase::BeginPlay()
-{
-	Super::BeginPlay();
-	SetUpLocalCustomPlayerName();
-}
-
-void APlayerCharacterBase::EndPlay(const EEndPlayReason::Type EndPlayReason)
-{
-	Super::EndPlay(EndPlayReason);
-}
-
-void APlayerCharacterBase::PossessedBy(AController* NewController)
-{
-	Super::PossessedBy(NewController);
-}
-
-float APlayerCharacterBase::TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser)
-{
-	AWizardGameState* GameState = GetWorld() ? GetWorld()->GetGameState<AWizardGameState>() : nullptr;
-	if (GameState)
-	{
-		GameState->DamageHealth(DamageAmount);
-		return DamageAmount;
-	}
-	
-	return Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
-}
-
-void APlayerCharacterBase::InterpolateCameraToLocation(FVector& TargetLocation, const float LerpDuration)
-{
-}
-
-void APlayerCharacterBase::InterpolateCameraToRotation(FRotator& TargetRotation, const float LerpDuration)
-{
-}
-
-void APlayerCharacterBase::Move(const FInputActionValue& Value)
-{
-	if (!bShouldUseMovementInput)
-	{
-		return;
-	}
-	const FVector2D MovementVector = Value.Get<FVector2D>();
-
-	const FVector Direction = GetActorForwardVector();
-	const FVector RightVector = GetActorRightVector();
-
-	AddMovementInput(Direction, MovementVector.Y);
-	AddMovementInput(RightVector, MovementVector.X);
-}
-
-void APlayerCharacterBase::Look(const FInputActionValue& Value)
-{
-	if (!bShouldUseLookInput)
-	{
-		return;
-	}
-	const FVector2D LookAxisVector = Value.Get<FVector2D>();
-
-	AddControllerYawInput(LookAxisVector.X);
-	AddControllerPitchInput(LookAxisVector.Y);
-}
-
-void APlayerCharacterBase::UseFirstAttackComponent()
-{
-	if (!FirstAttackComponent)
-	{
-		UE_LOG(PlayerBaseLog, Error, TEXT("APlayerCharacterBase::UseFirstAttackComponent, FirstAttackComp is Null"));
-		return;
-	}
-
-	GetFirstAttackComponent()->StartAttack();
-}
-
-void APlayerCharacterBase::UseSecondAttackComponent()
-{
-	if (!SecondAttackComponent)
-	{
-		UE_LOG(PlayerBaseLog, Error, TEXT("APlayerCharacterBase::UseSecondAttackComponent, SecondAttackComp is Null"));
-		return;
-	}
-	GetSecondAttackComponent()->StartAttack();
-}
-
-void APlayerCharacterBase::Interact(const FInputActionValue& Value)
-{
-	if (InteractorComponent)
-	{
-        InteractorComponent->Execute_OnInteract(InteractorComponent,InteractorComponent->GetTargetInteractable().GetObject());
-	}
-}
-
-void APlayerCharacterBase::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
-{
-	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
-	DOREPLIFETIME(APlayerCharacterBase, CustomPlayerName);
-	DOREPLIFETIME(APlayerCharacterBase, bChangedName);
-}
-
-void APlayerCharacterBase::OnRep_CustomPlayerName()
-{
-	if (!PlayerNameTagWidgetComponent)
-	{
-		return;
-	}
-	
-	if (!PlayerNameTagWidgetComponent->GetWidget())
-	{
-		FTimerHandle TimerHandle;
-		GetWorldTimerManager().SetTimer(TimerHandle, [this]()
-		{
-			OnRep_CustomPlayerName();
-		}, 0.1f, false);
-		return;
-	}
-	
-	if (UPlayerNameTagWidget* PlayerNameTagWidget = Cast<UPlayerNameTagWidget>(PlayerNameTagWidgetComponent->GetWidget()))
-	{
-		PlayerNameTagWidget->SetCustomPlayerName(FText::FromString(CustomPlayerName));
-	}
-}
-
-void APlayerCharacterBase::SetUpLocalCustomPlayerName()
-{
-	if (!bChangedName)
-	{
-#if WITH_EDITORONLY_DATA
-		if (!bUsePlayerLoginProfile)
-		{
-			const int32 RandomNum = FMath::RandRange(10, 99);
-			CustomPlayerName = FString::Printf(TEXT("Player_%d"), RandomNum);
-		}
-		else if (UPlayerLoginSystem* PlayerLoginSystem = GetGameInstance()->GetSubsystem<UPlayerLoginSystem>())
-		{
-			CustomPlayerName = PlayerLoginSystem->GetProfile().Username;
-		}
-#else
-		if (UPlayerLoginSystem* PlayerLoginSystem = GetGameInstance()->GetSubsystem<UPlayerLoginSystem>())
-		{
-			CustomPlayerName = PlayerLoginSystem->GetProfile().Username;
-		}
-#endif
-		bChangedName = true;
-}
-		if (IsLocallyControlled())
-		{
-			Server_SetCustomPlayerName(CustomPlayerName);
-
-		}
-		OnRep_CustomPlayerName();
-	
-}
-
-void APlayerCharacterBase::Multicast_SpawnHitParticles_Implementation()
-{
-}
-
-void APlayerCharacterBase::Server_SpawnHitParticles_Implementation()
-{
-}
-
-void APlayerCharacterBase::Server_SetCustomPlayerName_Implementation(const FString& InPlayerName)
-{
-	CustomPlayerName = InPlayerName;
-	OnRep_CustomPlayerName();
-}
-
-bool APlayerCharacterBase::Server_SetCustomPlayerName_Validate(const FString& InPlayerName)
-{
-	return true;
-}
-
 void APlayerCharacterBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 	TickNotLocal();
-}
-
-void APlayerCharacterBase::TickNotLocal()
-{
-	if (IsLocallyControlled())
-	{
-		return;
-	}
-	const FVector ComponentLocation = PlayerNameTagWidgetComponent->GetComponentLocation();
-	if (!GEngine)
-	{
-		return;
-	}
-	APlayerController* LocalPlayerController = GEngine->GetFirstLocalPlayerController(GetWorld());
-	if (LocalPlayerController && LocalPlayerController->PlayerCameraManager)
-	{
-		const FVector CameraLocation = LocalPlayerController->PlayerCameraManager->GetCameraLocation();
-		const FRotator FindLookAtRotation = UKismetMathLibrary::FindLookAtRotation(ComponentLocation, CameraLocation);
-		PlayerNameTagWidgetComponent->SetWorldRotation(FindLookAtRotation);
-	}
 }
 
 void APlayerCharacterBase::SetupPlayerInputComponent_Implementation(UInputComponent* PlayerInputComponent)
@@ -303,10 +111,9 @@ FVector APlayerCharacterBase::GetLeftHandSocketLocation() const
 
 AActor* APlayerCharacterBase::GetRightHandAttachedActor() const
 {
-	TArray<AActor*> AttachedActors;
-
 	if (GetMesh())
 	{
+		TArray<AActor*> AttachedActors;
 		GetMesh()->GetOwner()->GetAttachedActors(AttachedActors);
 		for (AActor* Attached: AttachedActors)
 		{
@@ -340,7 +147,7 @@ void APlayerCharacterBase::HandleCameraDetachment()
 	
 	bUseControllerRotationYaw = false;
 	bShouldUseLookInput = false;
-	bShouldUseMovementInput = false;
+	bShouldUseMoveInput = false;
 	
 	FollowCameraRelativeLocation = FollowCamera->GetRelativeLocation();
 	FollowCameraRelativeRotation = FollowCamera->GetRelativeRotation();
@@ -364,7 +171,7 @@ void APlayerCharacterBase::HandleCameraReattachment()
 	
 	bUseControllerRotationYaw = true;
 	bShouldUseLookInput = true;
-	bShouldUseMovementInput = true;
+	bShouldUseMoveInput = true;
 	
 	FollowCamera->AttachToComponent(CameraBoom, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
 	FollowCamera->SetRelativeLocationAndRotation(FollowCameraRelativeLocation, FollowCameraRelativeRotation);
@@ -382,5 +189,196 @@ void APlayerCharacterBase::InterpolateCamera(
 	FVector TargetLocation = TargetTransform.GetLocation();
 	
 	InterpolateCameraToLocation(TargetLocation, LerpDuration / 2.f);
+	
+}
+
+void APlayerCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	SetUpLocalCustomPlayerName();
+}
+
+void APlayerCharacterBase::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+}
+
+void APlayerCharacterBase::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+}
+
+void APlayerCharacterBase::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(APlayerCharacterBase, CustomPlayerName);
+	DOREPLIFETIME(APlayerCharacterBase, bChangedName);
+}
+
+float APlayerCharacterBase::TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser)
+{
+	if (AWizardGameState* GameState = GetWorld()->GetGameState<AWizardGameState>())
+	{
+		GameState->DamageHealth(DamageAmount);
+		return DamageAmount;
+	}
+	
+	return Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+}
+
+void APlayerCharacterBase::TickNotLocal()
+{
+	if (IsLocallyControlled())
+	{
+		return;
+	}
+	const FVector ComponentLocation = PlayerNameTagWidgetComponent->GetComponentLocation();
+	if (!GEngine)
+	{
+		return;
+	}
+	APlayerController* LocalPlayerController = GEngine->GetFirstLocalPlayerController(GetWorld());
+	if (LocalPlayerController && LocalPlayerController->PlayerCameraManager)
+	{
+		const FVector CameraLocation = LocalPlayerController->PlayerCameraManager->GetCameraLocation();
+		const FRotator FindLookAtRotation = UKismetMathLibrary::FindLookAtRotation(ComponentLocation, CameraLocation);
+		PlayerNameTagWidgetComponent->SetWorldRotation(FindLookAtRotation);
+	}
+}
+
+void APlayerCharacterBase::InterpolateCameraToLocation(FVector& TargetLocation, const float LerpDuration)
+{
+}
+
+void APlayerCharacterBase::InterpolateCameraToRotation(FRotator& TargetRotation, const float LerpDuration)
+{
+}
+
+void APlayerCharacterBase::Move(const FInputActionValue& Value)
+{
+	if (!bShouldUseMoveInput)
+	{
+		return;
+	}
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	const FVector Direction = GetActorForwardVector();
+	const FVector RightVector = GetActorRightVector();
+
+	AddMovementInput(Direction, MovementVector.Y);
+	AddMovementInput(RightVector, MovementVector.X);
+}
+
+void APlayerCharacterBase::Look(const FInputActionValue& Value)
+{
+	if (!bShouldUseLookInput)
+	{
+		return;
+	}
+	const FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	AddControllerYawInput(LookAxisVector.X);
+	AddControllerPitchInput(LookAxisVector.Y);
+}
+
+void APlayerCharacterBase::UseFirstAttackComponent()
+{
+	if (!FirstAttackComponent)
+	{
+		UE_LOG(PlayerBaseLog, Error, TEXT("APlayerCharacterBase::UseFirstAttackComponent, FirstAttackComp is Null"));
+		return;
+	}
+
+	GetFirstAttackComponent()->StartAttack();
+}
+
+void APlayerCharacterBase::UseSecondAttackComponent()
+{
+	if (!SecondAttackComponent)
+	{
+		UE_LOG(PlayerBaseLog, Error, TEXT("APlayerCharacterBase::UseSecondAttackComponent, SecondAttackComp is Null"));
+		return;
+	}
+	GetSecondAttackComponent()->StartAttack();
+}
+
+void APlayerCharacterBase::Interact(const FInputActionValue& Value)
+{
+	if (InteractorComponent)
+	{
+        InteractorComponent->Execute_OnInteract(InteractorComponent,InteractorComponent->GetTargetInteractable().GetObject());
+	}
+}
+
+void APlayerCharacterBase::Server_SpawnHitParticles_Implementation()
+{
+}
+
+void APlayerCharacterBase::Multicast_SpawnHitParticles_Implementation()
+{
+}
+
+void APlayerCharacterBase::OnRep_CustomPlayerName()
+{
+	if (!PlayerNameTagWidgetComponent)
+	{
+		return;
+	}
+	
+	if (!PlayerNameTagWidgetComponent->GetWidget())
+	{
+		FTimerHandle TimerHandle;
+		GetWorldTimerManager().SetTimer(TimerHandle, [this]()
+		{
+			OnRep_CustomPlayerName();
+		}, 0.1f, false);
+		return;
+	}
+	
+	if (UPlayerNameTagWidget* PlayerNameTagWidget = Cast<UPlayerNameTagWidget>(PlayerNameTagWidgetComponent->GetWidget()))
+	{
+		PlayerNameTagWidget->SetCustomPlayerName(FText::FromString(CustomPlayerName));
+	}
+}
+
+void APlayerCharacterBase::Server_SetCustomPlayerName_Implementation(const FString& InPlayerName)
+{
+	CustomPlayerName = InPlayerName;
+	OnRep_CustomPlayerName();
+}
+
+bool APlayerCharacterBase::Server_SetCustomPlayerName_Validate(const FString& InPlayerName)
+{
+	return true;
+}
+
+void APlayerCharacterBase::SetUpLocalCustomPlayerName()
+{
+	if (!bChangedName)
+	{
+#if WITH_EDITORONLY_DATA
+		if (!bUsePlayerLoginProfile)
+		{
+			const int32 RandomNum = FMath::RandRange(10, 99);
+			CustomPlayerName = FString::Printf(TEXT("Player_%d"), RandomNum);
+		}
+		else if (UPlayerLoginSystem* PlayerLoginSystem = GetGameInstance()->GetSubsystem<UPlayerLoginSystem>())
+		{
+			CustomPlayerName = PlayerLoginSystem->GetProfile().Username;
+		}
+#else
+		if (UPlayerLoginSystem* PlayerLoginSystem = GetGameInstance()->GetSubsystem<UPlayerLoginSystem>())
+		{
+			CustomPlayerName = PlayerLoginSystem->GetProfile().Username;
+		}
+#endif
+		bChangedName = true;
+}
+		if (IsLocallyControlled())
+		{
+			Server_SetCustomPlayerName(CustomPlayerName);
+
+		}
+		OnRep_CustomPlayerName();
 	
 }
--- Source/PROJ_2025/Player/Characters/PlayerCharacterBase.h
+++ Source/PROJ_2025/Player/Characters/PlayerCharacterBase.h
@@ -1,13 +1,12 @@
-﻿
-#pragma once
+﻿#pragma once
 
 #include "NiagaraFunctionLibrary.h"
-#include "NiagaraComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "PlayerCharacterBase.generated.h"
 
-
+class UCameraComponent;
+class USpringArmComponent;
 class UWidgetComponent;
 class UInteractorComponent;
 class UInputAction;
@@ -17,9 +16,6 @@ class UAttackComponentBase;
 
 DECLARE_LOG_CATEGORY_EXTERN(PlayerBaseLog, Log, All);
 
-class UCameraComponent;
-class USpringArmComponent;
-
 UCLASS()
 class PROJ_2025_API APlayerCharacterBase : public ACharacter
 {
@@ -27,22 +23,26 @@ class PROJ_2025_API APlayerCharacterBase : public ACharacter
 	
 public:
 	APlayerCharacterBase();
-
+	
+	//Handle override parent functions
 	virtual void Tick(float DeltaTime) override;
 
 	UFUNCTION(NetMulticast, Reliable)
 	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
-
+	
+	//Handle components
 	UAttackComponentBase* GetFirstAttackComponent() const;
 
 	UAttackComponentBase* GetSecondAttackComponent() const;
-
+	
+	//Handle sockets
 	FVector GetRightHandSocketLocation() const;
 
 	FVector GetLeftHandSocketLocation() const;
 	
 	AActor* GetRightHandAttachedActor() const;
-
+	
+	//Handle camera
 	virtual void HandleCameraDetachment();
 	
 	virtual void HandleCameraReattachment();
@@ -55,26 +55,30 @@ public:
 
 	FORCEINLINE UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 
+	//TODO: This may be moved to the belonging attack component
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Particles")
 	UNiagaraSystem* ImpactParticles;
 
 protected:
-	virtual void TickNotLocal();
-	
+	//Handle override parent functions
 	virtual void BeginPlay() override;
 
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
 	virtual void PossessedBy(AController* NewController) override;
+	
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
 
 	UFUNCTION(BlueprintCallable)
 	virtual float TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator,AActor* DamageCauser) override;
 	
-	//Components and Camera
+	//Handle nametag
+	virtual void TickNotLocal();
+	
+	//Handle camera
 	virtual void InterpolateCameraToLocation(FVector& TargetLocation, const float LerpDuration);
 	
 	virtual void InterpolateCameraToRotation(FRotator& TargetRotation, const float LerpDuration);
-	
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
 	USpringArmComponent* CameraBoom;
@@ -88,7 +92,7 @@ protected:
 	UPROPERTY()
 	FRotator FollowCameraRelativeRotation = FRotator::ZeroRotator;
 	
-	//Input
+	//Handle input
 	virtual void Move(const FInputActionValue& Value);
 
 	virtual void Look(const FInputActionValue& Value);
@@ -99,10 +103,9 @@ protected:
 
 	virtual void Interact(const FInputActionValue& Value);
 	
-	UPROPERTY()
 	bool bShouldUseLookInput = true;
 	
-	bool bShouldUseMovementInput = true;
+	bool bShouldUseMoveInput = true;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Input|Movement")
 	UInputAction* MoveAction;
@@ -122,6 +125,7 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Input|Misc")
 	UInputAction* InteractAction;
 
+	//Handle components
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Components|Ability")
 	UAttackComponentBase* FirstAttackComponent;
 
@@ -131,6 +135,7 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Components|Misc")
 	TObjectPtr<UInteractorComponent> InteractorComponent;
 	
+	//Handle nametag
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Components|Misc")
 	TObjectPtr<UWidgetComponent> PlayerNameTagWidgetComponent;
 
@@ -140,18 +145,21 @@ protected:
 	UPROPERTY(Replicated, VisibleAnywhere)
 	bool bChangedName = false;
 	
-	//Socket Names
+	//Handle sockets
 	UPROPERTY(VisibleAnywhere, Category="Socket Names")
 	FName RightHandSocket = TEXT("HandGrip_R");
 
 	UPROPERTY(VisibleAnywhere, Category="Socket Names")
 	FName LeftHandSocket = TEXT("HandGrip_L");
 	
-	
+	//TODO: Remove unused functions 
+	UFUNCTION(Server, Reliable)
+	void Server_SpawnHitParticles();
+
+	UFUNCTION(NetMulticast, Reliable)
+	void Multicast_SpawnHitParticles();
 private:
-	
-	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
-	
+	//Handle nametag	
 	UFUNCTION()
 	void OnRep_CustomPlayerName();
 	
@@ -161,12 +169,7 @@ private:
 	UFUNCTION(BlueprintCallable)
 	void SetUpLocalCustomPlayerName();
 
-	UFUNCTION(Server, Reliable)
-	void Server_SpawnHitParticles();
-
-	UFUNCTION(NetMulticast, Reliable)
-	void Multicast_SpawnHitParticles();
-	
+	//Handle editor debug
 #if WITH_EDITORONLY_DATA
 	
 	UPROPERTY(EditAnywhere, Category="Player Name Debug")
