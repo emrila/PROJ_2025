be102c858378feb66f90cc0f2d29aae647be38fc
Emma <86224710+emrila@users.noreply.github.com>
2025-11-20 10:40:50 +0100

    Add separation of UpgradeUtils

---
+++ Source/Upgrade/Private/Util/UpgradeUtil.cpp
@@ -0,0 +1,39 @@
+﻿#include "UpgradeUtil.h"
+#include "Core/AttributeData.h"
+
+FProperty* UpgradeUtils::GetProperty(const UObject* Owner, const FName PropertyName)
+{
+	if (!Owner)
+	{
+		return nullptr;
+	}
+
+	return Owner->GetClass()->FindPropertyByName(PropertyName);
+}
+
+bool UpgradeUtils::IsValidProperty(const FProperty* Property)
+{
+	return Property && (
+		Property->IsA<FFloatProperty>() ||
+		Property->IsA<FIntProperty>()
+	);
+}
+
+TUniquePtr<FAttributeBase> UpgradeUtils::CreateAttribute(UObject* InOwner, FProperty* InProperty, const FName InRowName)
+{
+	if (!InProperty)
+	{
+		return nullptr;
+	}
+
+	if (InProperty->IsA<FFloatProperty>())
+	{
+		return MakeUnique<FAttributeFloat>(InOwner, InProperty, InRowName);
+	}
+	if (InProperty->IsA<FIntProperty>())
+	{
+		return MakeUnique<FAttributeInt32>(InOwner, InProperty, InRowName);
+	}
+
+	return nullptr;
+}
