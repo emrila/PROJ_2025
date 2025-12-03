2664027963034c9cfb7635b1374bf9c46c5119b2
Emma <86224710+emrila@users.noreply.github.com>
2025-12-03 19:50:08 +0100

    Add PlayerHUDInterface

---
+++ Source/GUI/Public/Core/PlayerHUDInterface.h
@@ -0,0 +1,30 @@
+﻿// 🐲Furkan approves of this🐲
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "PlayerHUDInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI, Blueprintable, BlueprintType)
+class UPlayerHUDInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+
+class GUI_API IPlayerHUDInterface
+{
+	GENERATED_BODY()
+
+public:
+	
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void InitWidgets();
+	virtual void InitWidgets_Implementation(){};	
+	
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void ToggleWidget(int32 WidgetGroup = 0);
+	virtual void ToggleWidget_Implementation(int32 WidgetGroup){};
+};
