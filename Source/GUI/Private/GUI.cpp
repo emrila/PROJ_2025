74ecfc606826ef658d457f2237e7df289a811d59
Emma <86224710+emrila@users.noreply.github.com>
2025-12-03 14:28:17 +0100

    Add GUI module

---
+++ Source/GUI/Private/GUI.cpp
@@ -0,0 +1,17 @@
+﻿#include "GUI.h"
+
+#define LOCTEXT_NAMESPACE "FGUIModule"
+
+void FGUIModule::StartupModule()
+{
+    
+}
+
+void FGUIModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FGUIModule, GUI)
