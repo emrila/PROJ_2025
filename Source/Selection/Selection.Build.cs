311eaa0c5e2e4a7183ece726c62863d528f298d1
emrila <emmarichterlarsson@gmail.com>
2026-01-06 16:52:29 +0100

    Add: Selection module with GAS

---
+++ Source/Selection/Selection.Build.cs
@@ -0,0 +1,29 @@
+﻿using UnrealBuildTool;
+
+public class Selection : ModuleRules
+{
+    public Selection(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core", "CoreUObject",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Engine",
+                "Slate",
+                "SlateCore",
+                "GameplayAbilities",
+                "GameplayTags",
+                "GameplayTasks",
+                "NetCore"
+            }
+        );
+    }
+}
