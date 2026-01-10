88cc93f763b9cf169eb68d8f65c661151ed41cac
emrila <emmarichterlarsson@gmail.com>
2026-01-10 21:28:15 +0100

    Add: USelectableComponent for managing selectables selection and registration events

---
+++ Source/Selection/Private/Components/SelectableComponent.cpp
@@ -0,0 +1,77 @@
+﻿// 🐲Furkan approves of this🐲
+
+#include "Components/SelectableComponent.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "Async/Async_WaitGameplayEvent.h"
+#include "Utility/SelectionLog.h"
+
+void USelectableComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	WaitGameplayEvents.Add_GetRef(UAsync_WaitGameplayEvent::ActivateAndWaitGameplayEventToActor(GetOwner(), SelectionTag, false, false))->
+	EventReceived.AddDynamic(this, &USelectableComponent::Server_OnRequestSelection);
+
+	if (bAutoRegister && Execute_CanRegister(this))
+	{
+		Server_OnRegisterSelectable(FGameplayEventData());
+	}
+}
+
+void USelectableComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	if (bAutoRegister && Execute_CanRegister(this))
+	{
+		Server_OnUnregisterSelectable(FGameplayEventData());
+	}
+	Super::EndPlay(EndPlayReason);
+}
+
+void USelectableComponent::OnRegisterSelectable_Implementation(FInstancedStruct RegistrationData)
+{
+	FGameplayEventData Payload;
+	Payload.Instigator = GetOwner();
+	Payload.Target = GetOwner();
+	Payload.EventTag = RegistrationTag;
+
+	UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(GetTargetOwner(), Payload.EventTag, Payload);
+}
+
+void USelectableComponent::OnUnregisterSelectable_Implementation(FInstancedStruct UnregistrationData)
+{
+	FGameplayEventData Payload ;
+	Payload.Instigator = GetOwner();
+	Payload.Target = GetOwner();
+	Payload.EventTag = UnregistrationTag;
+	UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(GetTargetOwner(), Payload.EventTag, Payload);
+}
+
+bool USelectableComponent::CanRegister_Implementation() const
+{
+	const AActor* TargetOwner = GetTargetOwner();
+	return TargetOwner && TargetOwner->HasAuthority();
+}
+
+void USelectableComponent::OnRequestSelection_Implementation(FInstancedStruct RequestData)
+{
+	const FGameplayEventData* Payload = RequestData.GetPtr<FGameplayEventData>();
+	if (Payload->EventTag.MatchesTag(SelectTag))
+	{
+		SELECTION_DISPLAY(TEXT("🛜 Selectable %s received selection request."), *GetOwner()->GetName());
+		Execute_OnSelected(GetOwner(), RequestData);
+		return;
+	}
+	if (Payload->EventTag.MatchesTag(DeselectTag))
+	{
+		SELECTION_DISPLAY(TEXT("🛜 Selectable %s received deselection request."), *GetOwner()->GetName());
+		Execute_OnDeselected(GetOwner(), RequestData);
+	}
+}
+
+AActor* USelectableComponent::GetTargetOwner() const
+{
+	return bUseOwnersOwnerAsTarget && GetOwner()
+		       ? GetOwner()->GetOwner()
+		       : GetOwner();
+}
---
+++ Source/Selection/Public/Components/SelectableComponent.h
@@ -0,0 +1,37 @@
+﻿// 🐲Furkan approves of this🐲
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SelectionComponent.h"
+#include "SelectableComponent.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class SELECTION_API USelectableComponent : public USelectionComponent
+{
+	GENERATED_BODY()
+
+public:
+	USelectableComponent() = default;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+public:
+	virtual void OnRegisterSelectable_Implementation(FInstancedStruct RegistrationData) override;
+	virtual void OnUnregisterSelectable_Implementation(FInstancedStruct UnregistrationData) override;
+	virtual bool CanRegister_Implementation() const override;
+
+	virtual void OnRequestSelection_Implementation(FInstancedStruct RequestData) override;
+private:
+	AActor* GetTargetOwner() const;
+
+protected:
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= "Selectable")
+	bool bAutoRegister = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= "Selectable")
+	bool bUseOwnersOwnerAsTarget = true;
+};
