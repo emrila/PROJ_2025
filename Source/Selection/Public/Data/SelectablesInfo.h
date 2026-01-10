8663a038bb54715642cd0545daef7f882d86451e
emrila <emmarichterlarsson@gmail.com>
2026-01-10 21:18:21 +0100

    Add: separate (BP friendly) SelectablesInfo struct for sharing and viewing selectables state and current selectors

---
+++ Source/Selection/Public/Data/SelectablesInfo.h
@@ -0,0 +1,24 @@
+﻿// 🐲Furkan approves of this🐲
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SelectablesInfo.generated.h"
+
+USTRUCT(BlueprintType)
+struct FSelectablesInfo
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(BlueprintReadOnly)
+	UObject* Selectable = nullptr;
+
+	UPROPERTY(BlueprintReadOnly)
+	int32 TotalSelectors = 0;
+
+	UPROPERTY(BlueprintReadOnly)
+	TArray<UObject*> Selectors;
+
+	UPROPERTY(BlueprintReadOnly, meta = (Bitmask, BitmaskEnum = "/Script/Selection.ESelectionNotificationFlags"))
+	uint8 EvaluationFlags = 0;
+};
