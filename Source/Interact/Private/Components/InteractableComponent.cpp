528a468451df666c193dc908fddc292d927bf8b1
emrila <emmarichterlarsson@gmail.com>
2026-01-12 18:50:37 +0100

    Add: InteractableComponent

---
+++ Source/Interact/Private/Components/InteractableComponent.cpp
@@ -0,0 +1,119 @@
+﻿// 🐲Furkan approves of this🐲
+
+
+#include "Components/InteractableComponent.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "Async/Async_WaitGameplayEvent.h"
+#include "Dev/InteractLog.h"
+#include "Utility/Tags.h"
+
+UInteractableComponent::UInteractableComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+	InteractTag = EVENT_TAG_INTERACT;
+	ProximityTag = EVENT_TAG_PROXIMITY;
+	EnterTag = EVENT_TAG_PROXIMITY_ENTER;
+	ExitTag = EVENT_TAG_PROXIMITY_EXIT;
+}
+
+void UInteractableComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetOwner()->OnActorBeginOverlap.AddDynamic(this, &UInteractableComponent::OnActorBeginOverlap);
+	GetOwner()->OnActorEndOverlap.AddDynamic(this, &UInteractableComponent::OnActorEndOverlap);
+
+	WaitGameplayEvents.Add_GetRef(UAsync_WaitGameplayEvent::ActivateAndWaitGameplayEventToActor(GetOwner(), ProximityTag, false, false))->EventReceived.AddDynamic(
+	this, &UInteractableComponent::OnProximityEvent);
+}
+
+void UInteractableComponent::OnActorBeginOverlap([[maybe_unused]] AActor* OverlappedActor, AActor* OtherActor)
+{
+	OnOverlap(OtherActor, EnterTag);
+}
+
+void UInteractableComponent::OnActorEndOverlap([[maybe_unused]] AActor* OverlappedActor, AActor* OtherActor)
+{
+	OnOverlap(OtherActor, ExitTag);
+}
+
+void UInteractableComponent::OnComponentBeginOverlap([[maybe_unused]] UPrimitiveComponent* OverlappedComp, AActor* OtherActor, [[maybe_unused]] UPrimitiveComponent* OtherComp, [[maybe_unused]] int32 OtherBodyIndex, [[maybe_unused]] bool bFromSweep, [[maybe_unused]] const FHitResult& SweepResult)
+{
+	OnOverlap(OtherActor, EnterTag);
+}
+
+void UInteractableComponent::OnComponentEndOverlap([[maybe_unused]] UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, [[maybe_unused]] UPrimitiveComponent* OtherComp, [[maybe_unused]] int32 OtherBodyIndex)
+{
+	OnOverlap(OtherActor, ExitTag);
+}
+
+void UInteractableComponent::OnProximityEvent(FGameplayEventData Payload)
+{
+	if (Payload.EventTag.MatchesTag(EnterTag))
+	{
+		Execute_OnEnterInteractableRange(GetOwner(), FInstancedStruct::Make<FGameplayEventData>(Payload));
+	}
+	else if (Payload.EventTag.MatchesTag(ExitTag))
+	{
+		Execute_OnExitInteractableRange(GetOwner(), FInstancedStruct::Make<FGameplayEventData>(Payload));
+	}
+}
+
+void UInteractableComponent::OnOverlap(AActor* OtherActor, const FGameplayTag EventTag) const
+{
+	if (!OtherActor)
+	{
+		return;
+	}
+	if (const APawn* OverlappingPawn = Cast<APawn>(OtherActor);
+		OverlappingPawn && OverlappingPawn->IsLocallyControlled())
+	{
+		FGameplayEventData Payload;
+		Payload.Instigator = OtherActor;
+		Payload.Target = GetOwner();
+		Payload.EventTag = EventTag;
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(GetOwner(), Payload.EventTag, Payload);
+	}
+}
+
+AActor* UInteractableComponent::GetTargetOwner() const
+{
+	return bUseOwnersOwnerAsTarget && GetOwner() ? GetOwner()->GetOwner() : GetOwner();
+}
+
+void UInteractableComponent::Server_OnInteract_Implementation(UObject* Interactor)
+{
+	INTERACT_HI_FROM(__FUNCTION__);
+	if (bInteractionInputEnabled) // See declaration for "SetInteractionInputEnabled" for explanation
+	{
+		SetInteractionInputEnabled(false);
+		FTimerHandle TimerHandle;
+		const FTimerDelegate TimerDelegate = FTimerDelegate::CreateUObject(this, &UInteractableComponent::SetInteractionInputEnabled, true);
+		GetOwner()->GetWorldTimerManager().SetTimer(TimerHandle, TimerDelegate, 0.1f, false);
+	}
+	else
+	{
+		return;
+	}
+
+	FGameplayEventData Payload;
+	Payload.Instigator = nullptr;
+	Payload.Target = GetOwner();
+	Payload.EventTag = InteractTag;
+
+	if (AActor* PayloadInstigator = Cast<AActor>(Interactor))
+	{
+		Payload.Instigator = PayloadInstigator;
+	}
+	else if (const UActorComponent* PayloadInstigatorOwner = Cast<UActorComponent>(Interactor))
+	{
+		Payload.Instigator = PayloadInstigatorOwner->GetOwner();
+	}
+
+	if (Payload.Instigator)
+	{
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(GetTargetOwner(), Payload.EventTag, Payload);
+	}
+}
---
+++ Source/Interact/Public/Components/InteractableComponent.h
@@ -0,0 +1,76 @@
+﻿// 🐲Furkan approves of this🐲
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbilityTypes.h"
+#include "Components/ActorComponent.h"
+#include "Interfaces/Interactable.h"
+#include "InteractableComponent.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class INTERACT_API UInteractableComponent : public UActorComponent, public IInteractable //TODO: make inherit shape or sphere component to have collision
+{
+	GENERATED_BODY()
+
+public:
+	UInteractableComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	UFUNCTION(BlueprintCallable, Server, Reliable)
+    void Server_OnInteract(UObject* Interactor);
+
+	UFUNCTION()
+	void OnActorBeginOverlap(AActor* OverlappedActor, AActor* OtherActor);
+	
+	UFUNCTION()
+	void OnActorEndOverlap(AActor* OverlappedActor, AActor* OtherActor);
+
+	UFUNCTION()
+	void OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnComponentEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+protected:
+	UFUNCTION()
+	void OnProximityEvent(FGameplayEventData Payload);
+
+	void OnOverlap(AActor* OtherActor, FGameplayTag EventTag) const;
+
+private:
+	AActor* GetTargetOwner() const;
+
+protected:
+	UPROPERTY()
+	TArray<class UAsync_WaitGameplayEvent*> WaitGameplayEvents;
+
+	/* Issue: "doubled up OnPress events for the same keypress under some conditions"
+	 * Post: https://forums.unrealengine.com/t/inputcomponent-bindings-sometimes-trigger-twice/2532480/2
+	 * Fix in 5.7.1: https://github.com/EpicGames/UnrealEngine/commit/44c2dcbda88c526e08987893e897f091d26781d9*/
+	UFUNCTION()
+	void SetInteractionInputEnabled(const bool bEnabled)
+	{
+		bInteractionInputEnabled = bEnabled;
+	}
+	bool bInteractionInputEnabled = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= "Interaction")
+	bool bUseOwnersOwnerAsTarget = true;
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category= "Interaction")
+	FGameplayTag InteractTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category= "Interaction")
+	FGameplayTag ProximityTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category= "Interaction")
+	FGameplayTag EnterTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category= "Interaction")
+	FGameplayTag ExitTag;
+};
