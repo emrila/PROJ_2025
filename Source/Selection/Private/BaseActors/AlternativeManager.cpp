b2a573640b990d9003c61484ca6df51084e51144
emrila <emmarichterlarsson@gmail.com>
2026-01-12 19:08:35 +0100

    Add: Base class for managing any group of selectable and interactable actors

---
+++ Source/Selection/Private/BaseActors/AlternativeManager.cpp
@@ -0,0 +1,62 @@
+﻿// 🐲Furkan approves of this🐲
+
+#include "BaseActors/AlternativeManager.h"
+
+#include "AbilitySystemComponent.h"
+#include "Components/ValidationComponent.h"
+#include "GameplayUtils/Public/Async/Async_WaitGameplayEvent.h"
+#include "GameplayUtils/Public/Utility/Tags.h"
+#include "Net/UnrealNetwork.h"
+
+AAlternativeManager::AAlternativeManager()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.TickInterval = 1.0f;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	bReplicates = true;
+
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+
+	ValidationComponent = CreateDefaultSubobject<UValidationComponent>(TEXT("ValidationComponent"));
+	ValidationComponent->SetIsReplicated(true);
+}
+
+UAbilitySystemComponent* AAlternativeManager::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void AAlternativeManager::BeginPlay()
+{
+	Super::BeginPlay();
+
+	WaitGameplayEvents.Add_GetRef(UAsync_WaitGameplayEvent::ActivateAndWaitGameplayEventToActor(this, EVENT_TAG_INTERACT, false, false))->
+	EventReceived.AddDynamic(this, &AAlternativeManager::WrapperOnInteractionEvent);
+}
+
+void AAlternativeManager::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(AAlternativeManager, AbilitySystemComponent);
+	DOREPLIFETIME(AAlternativeManager, ValidationComponent);
+}
+
+void AAlternativeManager::WrapperOnInteractionEvent(FGameplayEventData Payload)
+{
+	Execute_OnRequestSelection(ValidationComponent, FInstancedStruct::Make<FGameplayEventData>(Payload));
+}
+
+FInstancedStruct AAlternativeManager::OnGetSelectablesInfo_Implementation(const UObject* Selectable)
+{
+	if (ValidationComponent)
+	{
+		return FInstancedStruct::Make<FSelectablesInfos>();
+	}
+	return Selectable
+	? FInstancedStruct::Make<FSelectablesInfo>(ValidationComponent->GetSelectableInfo(Selectable))
+	: FInstancedStruct::Make<FSelectablesInfos>(ValidationComponent->GetAllSelectablesInfo());
+
+}
---
+++ Source/Selection/Public/BaseActors/AlternativeManager.h
@@ -0,0 +1,46 @@
+﻿// 🐲Furkan approves of this🐲
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "Abilities/GameplayAbilityTypes.h"
+#include "GameFramework/Actor.h"
+#include "Interfaces/Selectable.h"
+#include "Interfaces/SelectionDisplayInterface.h"
+#include "AlternativeManager.generated.h"
+
+class UAsync_WaitGameplayEvent;
+class UValidationComponent;
+class UAsyncWaitGameplayEvent;
+
+UCLASS()
+class SELECTION_API AAlternativeManager : public AActor, public IAbilitySystemInterface, public ISelectable, public ISelectionDisplayInterface
+{
+	GENERATED_BODY()
+
+public:
+	AAlternativeManager();
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+public:
+	UFUNCTION()
+	void WrapperOnInteractionEvent(FGameplayEventData Payload);
+
+	virtual FInstancedStruct OnGetSelectablesInfo_Implementation(const UObject* Selectable = nullptr) override;
+
+protected:
+	UPROPERTY(Replicated, VisibleAnywhere, BlueprintReadOnly, Category= "Ability System")
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY(Replicated, VisibleAnywhere, BlueprintReadOnly, Category= "Ability System")
+	TObjectPtr<UValidationComponent> ValidationComponent;
+
+	UPROPERTY()
+	TArray<UAsync_WaitGameplayEvent*> WaitGameplayEvents;
+
+};
